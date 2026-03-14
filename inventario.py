293e926c78e179800ef3ca8c66689a50ff31276f
mateoriwi <sharonmilena.mmm@gmail.com>
2026-03-13 20:44:35 -0500

    Fix indentation and comment formatting in inventario.py

--- inventario.py
+++ inventario.py
@@ -1,7 +1,7 @@
    #Aquí le pedimos el nombre del producto al usuario
     nombre_producto = input("Ingrese el nombre del producto: \n")
-#Este while true nos permite en un bucle, seguir en la misma función en caso de que ingresemos algo inválido, en precio del producto
 
+#Este while true nos permite en un bucle, seguir en la misma función en caso de que ingresemos algo inválido, en precio del producto
 while True:
     try:                  #Aquí convertimos el precio del producto en un float para que solo se puedan ingresar números, y en caso de una letra, ir al except
         precio_producto = float(input("Ingrese el precio del producto: \n"))
